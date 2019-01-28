dd5412b7154da2c76fef5d2ba31fcbf0cf8222d7
Francisco Lopez Muñoz <frans.lopez.m@gmail.com>
2019-01-28 18:54:11 +0100

    Add description, price and imageUrl to products model

--- models/products.js
+++ models/products.js
@@ -13,8 +13,11 @@ const getProductsFromFile = cb => {
 
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
