e6e2f67e43188c92694fd373428cf972d28b11ec
Francisco Lopez Muñoz <frans.lopez.m@gmail.com>
2019-02-02 19:37:26 +0100

    Add route for edit and remove products Add route show product details, delete and add product to a cart

--- routes/admin.js
+++ routes/admin.js
@@ -4,11 +4,13 @@ const adminCtrl = require('../controllers/admin');
 const router = Router();
 
 router.get('/add-product', adminCtrl.getProduct);
-
 router.post('/add-product', adminCtrl.postAddProduct);
 
 router.get('/products', adminCtrl.getProducts);
 
+router.get('/edit-product/:productId', adminCtrl.getEditProduct);
+router.post('/edit-product', adminCtrl.postEditProduct);
 
+router.post('/delete-product', adminCtrl.postDeleteProduct);
 
 module.exports = { router };
--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,12 @@ const router = Router();
 router.get('/', shopCtrl.getIndex);
 
 router.get('/products', shopCtrl.getProducts);
+router.get('/products/:productId', shopCtrl.getProduct);
 
 router.get('/cart', shopCtrl.getCart);
+router.post('/cart', shopCtrl.postCart);
+
+router.post('/cart-delete-item', shopCtrl.postCartDeleteProduct);
 
 router.get('/orders', shopCtrl.getOrders);
 
