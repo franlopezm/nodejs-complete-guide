45c1c1374af4dbd5f681c7bb748cc2b2e3305ecf
Francisco Lopez Muñoz <frans.lopez.m@gmail.com>
2019-02-02 19:35:32 +0100

    Update model Product

--- models/products.js
+++ models/products.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { getPathView } = require('../utils/path');
+const Cart = require('./cart');
 
 const pathData = getPathView('products.json', 'data');
 
@@ -13,7 +14,8 @@ const getProductsFromFile = cb => {
 
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -22,16 +24,50 @@ module.exports = class Product {
 
   save() {
     getProductsFromFile(products => {
-      products.push(this);
+      const newProducts = [...products];
 
-      fs.writeFile(pathData, JSON.stringify(products), (error) => {
+      if (this.id) {
+        // Update
+        const index = products.findIndex(p => p.id === this.id);
+
+        newProducts[index] = this;
+      } else {
+        // Add new
+        this.id = Math.random().toString();
+
+        newProducts.push(this);
+      }
+
+      fs.writeFile(pathData, JSON.stringify(newProducts), (error) => {
         console.log('TCL: Product -> save -> error', error)
       });
-
     });
   }
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+
+      cb(product);
+    });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      const updateProducts = products.filter(p => p.id !== id);
+
+      fs.writeFile(pathData, JSON.stringify(updateProducts), (error) => {
+        if (!error) {
+          Cart.deleteProduct(id, product.price);
+        } else {
+          console.log('TCL: Product -> save -> error', error)
+        }
+      });
+    });
   }
 }
