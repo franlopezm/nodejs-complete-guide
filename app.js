711987b46d10b42f133b73cc101378061332c96c
Francisco Lopez Muñoz <frans.lopez.m@gmail.com>
2019-02-16 15:27:34 +0100

    Install mongoose and added connection to ddbb

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { getPathView } = require('./utils/path');
 
-const sequelize = require('./utils/database');
-const Product = require('./models/products');
+/*
 const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+*/
 
-const { get404 } = require('./controllers/error');
-const adminRoutes = require('./routes/admin').router;
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { get404 } = require('./controllers/error');
 
 const app = express();
 // Remove Headers by security
@@ -27,55 +24,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Get user to available in applications
 app.use((req, res, next) => {
-  User.findByPk(1)
+  /* User.findByPk(1)
     .then(user => {
       // return a sequelize object
       req.user = user;
 
       next();
     })
-    .catch(error => console.log(error));
+    .catch(error => console.log(error)); */
 })
 
 // Routes
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 app.use(get404);
 
-// Relations between tables
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
 
 
-//Init database models, create tables and relations
-sequelize
-  //.sync({ force: true })
-  .sync()
-  .then(result => {
-    return User.findByPk(1);
-    // console.log(result);
-  })
-  .then(user => {
-    if (!user) {
-      return User.create({ name: 'Fran', email: 'test@test.com' });
-    }
-
-    return user;
-  })
-  .then(user => {
-    //console.log('TCL: user', user)
-    // Listen
-    return user.createCart();
-  })
-  .then(cart => {
+// Connect to ddbb and run app
+mongoose
+  .connect('mongodb://localhost:27017', { dbName: 'node-complete', autoIndex: false, useNewUrlParser: true })
+  .then(() => {
     app.listen(3000, () => console.log('Listening in PORT 3000'));
   })
-  .catch(error => console.log('Init database error:', error));
+  .catch(error => console.log('Connection ddbb Error::', error));
